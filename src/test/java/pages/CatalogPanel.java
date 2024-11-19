d54f52ec5393b65e5b3ea1cbe88a503964ad7f4b
Vladimir Sarychev <svmyhome@mail.ru>
2024-11-19 21:34:51 +0300

    add empty branch

--- src/test/java/pages/CatalogPanel.java
+++ src/test/java/pages/CatalogPanel.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Condition.disabled;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -6,7 +6,7 @@ import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.disabled;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
     private final SelenideElement loginButton = $("[automation-id='login-button']"),
