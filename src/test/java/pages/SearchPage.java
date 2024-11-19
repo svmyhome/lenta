df0c2bbaaa8fdc98da8ecf4e3a128cdbbe8681cb
Vladimir Sarychev <svmyhome@mail.ru>
2024-11-19 21:46:28 +0300

    fix

--- src/test/java/pages/SearchPage.java
+++ src/test/java/pages/SearchPage.java
@@ -9,7 +9,6 @@ import static com.codeborne.selenide.Selenide.$;
 public class SearchPage {
     private final static SelenideElement searchTitle = $(".head h1");
 
-
     @Step("Переход на страницу с результатами поиска")
     public SearchPage getTitle(String value) {
         searchTitle.shouldHave(text(value));
