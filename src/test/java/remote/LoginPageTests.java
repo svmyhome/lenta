b65da6a22dba936beedbc6b8aaa6df30728ecbf7
Vladimir Sarychev <svmyhome@mail.ru>
2025-02-01 19:17:57 +0300

    add remote

---
+++ src/test/java/remote/LoginPageTests.java
@@ -0,0 +1,32 @@
+package remote;
+
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import pages.LoginPage;
+import pages.MainPage;
+
+@Feature("Авторизация")
+@Story("Авторизоваться через кнопку")
+@Owner("sarychev")
+@Tags({@Tag("AUTHORIZATION"), @Tag("SMOKE")})
+public class LoginPageTests extends TestBaseRemote {
+
+    MainPage mainPage = new MainPage();;
+    LoginPage loginPage = new LoginPage();;
+
+    @Test
+    @DisplayName("Выполнен ввод номера телефона кнопка получить код активна")
+    @Severity(SeverityLevel.BLOCKER)
+    void LoginTest() {
+        mainPage.openMainPage();
+        loginPage.clickLoginButton()
+                .shouldAuthorizationWindowOpen()
+                .shouldCodeButtonDisable()
+                .setPhoneNumber()
+                .shouldCodeButtonEnable();
+
+    }
+}
