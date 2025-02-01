5df76b93d71b123f334d3375eca23205ded6ae73
Vladimir Sarychev <svmyhome@mail.ru>
2025-02-01 20:23:28 +0300

    add remote config

--- src/test/java/remote/TestBaseRemote.java
+++ src/test/java/remote/TestBaseRemote.java
@@ -8,8 +8,10 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.ByteArrayInputStream;
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.screenshot;
 import static com.codeborne.selenide.Selenide.webdriver;
@@ -26,6 +28,19 @@ public class TestBaseRemote {
 
 //        Configuration.pageLoadStrategy = "eager";
 //        Configuration.holdBrowserOpen = true;
+
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "122.0";
+        Configuration.baseUrl = "https://lenta.com/";
+//        Configuration.remote = "http://192.168.31.143:4444/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", false
+        ));
+        Configuration.browserCapabilities = capabilities;
+
     }
     @AfterEach
     void afterEach() {
