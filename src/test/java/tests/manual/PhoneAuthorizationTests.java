40c9fb248d8c8666b7c3a8eb3c9e4b1a1719e796
Vladimir Sarychev <svmyhome@mail.ru>
2025-03-11 21:49:20 +0300

    add manual tests

---
+++ src/test/java/tests/manual/PhoneAuthorizationTests.java
@@ -0,0 +1,48 @@
+package tests.manual;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qameta.allure.Layer;
+import qameta.allure.Manual;
+
+import static io.qameta.allure.Allure.step;
+
+@Layer("web")
+@Feature("Авторизация")
+@Story("Пользователь может авторизоваться через кнопку 'Войти'")
+@Manual
+@Owner("Sidorov")
+@DisplayName("Авторизация через номер телефона")
+public class PhoneAuthorizationTests {
+
+    @Test
+    @Manual
+    @DisplayName("Успешная авторизация через СМС")
+    public void successfulAuthorizeFromSmsTest() {
+        step("Открыть главную страницу Lenta.com");
+        step("Ввести авторизационные данные", () -> {
+            step("Ввести телефон");
+            step("Ввести пароль");
+        });
+        step("Кликнуть получить смс");
+        step("Ввести смс в поле ввода");
+        step("Переход в профиль пользователя выполнен");
+    }
+
+    @Test
+    @Manual
+    @DisplayName("Безуспешная авторизация через СМС, неверный смс код")
+    public void unsuccessfulAuthorizeFromWrongSmsTest() {
+        step("Открыть главную страницу Lenta.com");
+        step("Ввести авторизационные данные", () -> {
+            step("Ввести телефон");
+            step("Ввести пароль");
+        });
+        step("Кликнуть получить смс");
+        step("Ввести неверный смс в поле ввода");
+        step("Вы ввели неверный смс, повторите ввод");
+    }
+}
